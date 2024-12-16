48c1ca9c4b8872e44814e9872025dd89ba1b32a9
Jesiee <solante.jescel72@gmail.com>
2024-12-16 13:33:09 +0800

    Update conn.php

--- app1_jescel/conn.php
+++ app1_jescel/conn.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', '');
+  define('DB_USERNAME', 'jescel');
+  define('DB_PASSWORD', '1234');
   define('DB_NAME', 'student');
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
